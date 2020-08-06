b43110a9bfea8f9beeb3c26ed9e4a5f3a207c26d
fuda <futsurdmitriy@gmail.com>
2020-08-07 01:04:46 +0300

    Commit boolean operations.

---
+++ java_technology_boolean_operations/src/main/java/fuda/edu/Main.java
@@ -0,0 +1,15 @@
+package fuda.edu;/*
+ @author Dmitriy Futsur
+ @since 07 Aug 2020
+ @version 1.0.0 
+ Copyright (c) Dmitriy Futsur 
+ Description:
+
+ Create tests for boolean operations
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
