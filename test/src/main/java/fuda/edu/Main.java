43c593ccbf29ef4c74653049c4646315ea058366
fuda <futsurdmitriy@gmail.com>
2020-08-06 23:49:38 +0300

    Commit palindrome task (part_2).

--- test/src/main/java/fuda/edu/Main.java
+++ test/src/main/java/fuda/edu/Main.java
@@ -1,15 +1,38 @@
+/*
+ * Class name :  Main
+ *
+ * @author Dmitriy Futsur
+ *
+ * @version 1.0.0 06-Aug-2020
+ *
+ * Copyright (c) Dmitriy Futsur
+ *
+ * Description:
+ *
+ * 1) Implement static method isValid(...){...}
+ * 2) Logic of method isValid(...){...} has to be encapsulated
+ * 3) Implement logger and test for each method of class PalindromeService
+ * 4) Implement cyrillic symbols check
+ *
+ */
+
 package fuda.edu;
 
+/**
+ * This is Main class
+ * @author Dmitriy Futsur
+ * @version 1.0.0 06-Aug-2020
+ */
 public class Main {
+
+    /**
+     * main method of Main class
+     * @param args This is default param for main method
+     */
     public static void main(String[] args) {
-//        String abba = "ABbA";
-//        PalindromeService palindromeService = new PalindromeService(abba);
-//        System.out.println(palindromeService.isValid());
+
+        // Call of static method isValid(...){...}
         System.out.println(PalindromeService.isValid("AbBa"));
 
-        // 1) static method
-        // 2) encapsulated logic to check palindrome
-        // 3) logger for each method and test for each method
-        // 4) cyrillic check also implement
     }
 }
--- test/src/main/java/fuda/edu/PalindromeService.java
+++ test/src/main/java/fuda/edu/PalindromeService.java
@@ -1,5 +1,28 @@
+/*
+ * Class name :  PalindromeService
+ *
+ * @author Dmitriy Futsur
+ *
+ * @version 1.0.0 06-Aug-2020
+ *
+ * Copyright (c) Dmitriy Futsur
+ *
+ * Description:
+ *
+ * 1) Implement static method isValid(...){...}
+ * 2) Logic of method isValid(...){...} has to be encapsulated
+ * 3) Implement logger and test for each method of class PalindromeService
+ * 4) Implement cyrillic symbols check
+ *
+ */
+
 package fuda.edu;
 
+/**
+ * Class to process string and define whether it palindrome or not
+ * @author Dmitriy Futsur
+ * @version 1.0.0 06-Aug-2020
+ */
 public class PalindromeService {
 
     private String string;
@@ -32,18 +55,12 @@ public class PalindromeService {
         return new StringBuilder (string).reverse().toString();
     }
 
-    /*public Boolean isValid(){
-        toLowerCase();
-        getCleanedString();
-        return this.getString().equals(this.getReversed());
-    }*/
-
     public static Boolean isValid(String string) {
         if (string == null) return null;
         PalindromeService palindromeService = new PalindromeService(string);
         palindromeService.toLowerCase();
         palindromeService.getCleanedString();
-        if (palindromeService.getString().length() == 0) return null;
+        if (palindromeService.getString().length() == 0) return false;
         return palindromeService.getString().equals(palindromeService.getReversed());
     }
 }
