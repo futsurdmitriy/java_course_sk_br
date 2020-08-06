3765c17b83bb6ef83bb87da1524412cfd02ac37b
fuda <futsurdmitriy@gmail.com>
2020-08-07 00:15:25 +0300

    Commit java_technology_palindrome task - finished.

--- java_technology_palindrome/src/main/java/fuda/edu/PalindromeService.java
+++ java_technology_palindrome/src/main/java/fuda/edu/PalindromeService.java
@@ -25,36 +25,73 @@ package fuda.edu;
  */
 public class PalindromeService {
 
+    /**
+     *  Field for string to check
+     */
     private String string;
 
-    // regex to clean non alphabetic and non cyrillic symbols
+    /**
+     * Regular expression to remove all the symbols except
+     * alphabetic and cyrillic symbols
+     */
     private static final String regex = "[^a-zA-Zа-яА-Я]";
 
+    /**
+     * Getter for string to check
+     * @return String type string
+     */
     public String getString() {
         return string;
     }
 
+    /**
+     * Setter for string to check
+     * @param string String to check
+     */
     public void setString(String string) {
         this.string = string;
     }
 
+    /**
+     * Empty constructor
+     */
     public PalindromeService() {
     }
 
+    /**
+     * Constructor with parameter
+     * @param string String to check
+     */
     public PalindromeService(String string) { this.string = string;}
 
+    /**
+     * Method to convert string to lower case
+     */
     private void toLowerCase() {
         this.setString(this.string.toLowerCase());
     }
 
+    /**
+     * Method to get formatted string free from symbols and spaces
+     */
     private void getCleanedString(){
         this.setString(this.string.replaceAll(regex, ""));
     }
 
+    /**
+     * Method to get reversed string to check
+     * @return Returns reversed string
+     */
     private String getReversed(){
         return new StringBuilder (string).reverse().toString();
     }
 
+    /**
+     * Static implementation to check whether string is palindrome or not
+     * @param string String to check
+     * @return Returns boolean true if string is palindrome false if not and
+     * null if string is undefined
+     */
     public static Boolean isValid(String string) {
         if (string == null) return null;
         PalindromeService palindromeService = new PalindromeService(string);
